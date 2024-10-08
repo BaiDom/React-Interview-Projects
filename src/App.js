db29125a0868d82da3939681195539ae8f2e8e04
BaiDom <dombaily88@gmail.com>
2024-10-08 15:49:48 +0100

    adding image slider project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -7,13 +8,20 @@ function App() {
   return (
     <div className="App">
       {/* Accordian component*/}
-      <Accordian />
+      {/* <Accordian /> */}
 
       {/*random Color component*/}
-      <RandomColor />
+      {/* <RandomColor /> */}
 
       {/* Star Rating Component*/}
-      {<StarRating noOfStars={10} />}
+      {/* {<StarRating noOfStars={10} />} */}
+
+      {/* Image Slide Component*/}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
